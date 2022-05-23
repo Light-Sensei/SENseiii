58320c7bbea8901ca8f47a21d864f298b3b6d3c9
Light Yagami <99256366+Light-Sensei@users.noreply.github.com>
2022-05-23 18:14:22 +0530

    Create script.py

---
+++ SiestaRobot/script.py
@@ -0,0 +1,6 @@
+MIKU_IMG = (
+      "https://telegra.ph/file/504b453954329a9cf3d76.jpg",
+      "https://telegra.ph/file/15a39f910986b639a4981.jpg",
+      "https://telegra.ph/file/5fc0fa0316da7ad8ed53a.jpg",
+      "https://telegra.ph/file/ee373f53bf79e8681e1b8.jpg",
+)
