eeafea849632f898fac203d90e1f6d2bc8c6f2f6
Makima <105355430+Control-Devil@users.noreply.github.com>
2022-06-08 14:16:10 +0530

    trying

--- SiestaRobot/modules/helper_funcs/misc.py
+++ SiestaRobot/modules/helper_funcs/misc.py
@@ -1,12 +1,19 @@
-import os
-import cv2
-from typing import Dict, List, Union
-from html import escape
-import ffmpeg
+from math import ceil
+from typing import Dict, List
+from uuid import uuid4
+
+from telegram import (
+    MAX_MESSAGE_LENGTH,
+    Bot,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    InlineQueryResultArticle,
+    InputTextMessageContent,
+    ParseMode,
+)
+from telegram.error import TelegramError
 
 from SiestaRobot import NO_LOAD
-from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
-from telegram.error import TelegramError
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
@@ -47,11 +54,11 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
                 EqInlineKeyboardButton(
                     x.__mod_name__,
                     callback_data="{}_module({})".format(
-                        prefix, x.__mod_name__.lower(),
+                        prefix, x.__mod_name__.lower()
                     ),
                 )
                 for x in module_dict.values()
-            ],
+            ]
         )
     else:
         modules = sorted(
@@ -59,11 +66,11 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
                 EqInlineKeyboardButton(
                     x.__mod_name__,
                     callback_data="{}_module({},{})".format(
-                        prefix, chat, x.__mod_name__.lower(),
+                        prefix, chat, x.__mod_name__.lower()
                     ),
                 )
                 for x in module_dict.values()
-            ],
+            ]
         )
 
     pairs = [modules[i * 3 : (i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)]
@@ -72,11 +79,54 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     calc = len(modules) - round(round_num)
     if calc in [1, 2]:
         pairs.append((modules[-1],))
+
+    max_num_pages = ceil(len(pairs) / 6)
+    modulo_page = page_n % max_num_pages
+
+    # can only have a certain amount of buttons side by side
+    if len(pairs) > 3:
+        pairs = pairs[modulo_page * 6 : 6 * (modulo_page + 1)] + [
+            (
+                EqInlineKeyboardButton(
+                    "❮", callback_data="{}_prev({})".format(prefix, modulo_page)
+                ),
+                EqInlineKeyboardButton("Back", callback_data="shikimori_back"),
+                EqInlineKeyboardButton(
+                    "❯", callback_data="{}_next({})".format(prefix, modulo_page)
+                ),
+            )
+        ]
+
+    else:
+        pairs += [[EqInlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="shikimori_back")]]
+
     return pairs
 
 
+def article(
+    title: str = "",
+    description: str = "",
+    message_text: str = "",
+    thumb_url: str = None,
+    reply_markup: InlineKeyboardMarkup = None,
+    disable_web_page_preview: bool = False,
+) -> InlineQueryResultArticle:
+
+    return InlineQueryResultArticle(
+        id=uuid4(),
+        title=title,
+        description=description,
+        thumb_url=thumb_url,
+        input_message_content=InputTextMessageContent(
+            message_text=message_text,
+            disable_web_page_preview=disable_web_page_preview,
+        ),
+        reply_markup=reply_markup,
+    )
+
+
 def send_to_list(
-    bot: Bot, send_to: list, message: str, markdown=False, html=False,
+    bot: Bot, send_to: list, message: str, markdown=False, html=False
 ) -> None:
     if html and markdown:
         raise Exception("Can only send with either markdown or HTML!")
@@ -127,54 +177,28 @@ def build_keyboard_parser(bot, chat_id, buttons):
     return keyb
 
 
+def user_bot_owner(func):
+    @wraps(func)
+    def is_user_bot_owner(bot: Bot, update: Update, *args, **kwargs):
+        user = update.effective_user
+        if user and user.id == OWNER_ID:
+            return func(bot, update, *args, **kwargs)
+        else:
+            pass
+
+    return is_user_bot_owner
+
+
+def build_keyboard_alternate(buttons):
+    keyb = []
+    for btn in buttons:
+        if btn[2] and keyb:
+            keyb[-1].append(InlineKeyboardButton(btn[0], url=btn[1]))
+        else:
+            keyb.append([InlineKeyboardButton(btn[0], url=btn[1])])
+
+    return keyb
+
+
 def is_module_loaded(name):
     return name not in NO_LOAD
-
-#function to mention username for chats https://t.me/username
-def mention_username(username: str, name: str) -> str:
-    """
-    Args:
-        username (:obj:`str`): The username of chat which you want to mention.
-        name (:obj:`str`): The name the mention is showing.
-    Returns:
-        :obj:`str`: The inline mention for the user as HTML.
-    """
-    return f'<a href="t.me/{username}">{escape(name)}</a>'
-
-def convert_gif(input):
-    """Function to convert mp4 to webm(vp9)"""
-
-    vid = cv2.VideoCapture(input)
-    height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
-
-    #check height and width to scale
-    if width > height:
-        width = 512
-        height = -1
-    elif height > width:
-        height = 512
-        width = -1
-    elif width == height:
-        width = 512
-        height = 512
-
-
-    converted_name = 'kangsticker.webm'
-
-    (
-        ffmpeg
-            .input(input)
-            .filter('fps', fps=30, round="up")
-            .filter('scale', width=width, height=height)
-            .trim(start="00:00:00", end="00:00:03", duration="3")
-            .output(converted_name, vcodec="libvpx-vp9", 
-                        **{
-                            #'vf': 'scale=512:-1',
-                            'crf': '30'
-                            })
-            .overwrite_output()
-            .run()
-    )
-
-    return converted_name
