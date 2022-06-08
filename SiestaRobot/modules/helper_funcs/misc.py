bbdef8d0ea8a29f7d8ed987f5612f1173bd92618
Makima <105355430+Control-Devil@users.noreply.github.com>
2022-06-08 15:08:11 +0530

    fixed

--- SiestaRobot/modules/helper_funcs/misc.py
+++ SiestaRobot/modules/helper_funcs/misc.py
@@ -1,15 +1,19 @@
-from typing import Dict, List
-import typing
-from uuid import uuid4
-from SiestaRobot import NO_LOAD
-from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, InlineQueryResultArticle, InputTextMessageContent
-from telegram.error import TelegramError
-import requests
-import json
-import zlib
-import base64
-from urllib.parse import urlparse, urljoin, urlunparse
 from math import ceil
+from typing import Dict, List
+from uuid import uuid4
+
+from telegram import (
+    MAX_MESSAGE_LENGTH,
+    Bot,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    InlineQueryResultArticle,
+    InputTextMessageContent,
+    ParseMode,
+)
+from telegram.error import TelegramError
+
+from SiestaRobot import NO_LOAD
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
@@ -36,75 +40,59 @@ def split_message(msg: str) -> List[str]:
         else:
             result.append(small_msg)
             small_msg = line
-    # Else statement at the end of the for loop, so append the leftover string.
-    result.append(small_msg)
+    else:
+        # Else statement at the end of the for loop, so append the leftover string.
+        result.append(small_msg)
 
     return result
 
 
 def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if not chat:
-            modules = sorted(
+        modules = sorted(
             [
                 EqInlineKeyboardButton(
                     x.__mod_name__,
                     callback_data="{}_module({})".format(
-                        prefix, x.__mod_name__.replace(" ", "_").lower()
+                        prefix, x.__mod_name__.lower()
                     ),
                 )
                 for x in module_dict.values()
             ]
         )
     else:
-            modules = sorted(
-                 [
+        modules = sorted(
+            [
                 EqInlineKeyboardButton(
                     x.__mod_name__,
                     callback_data="{}_module({},{})".format(
-                        prefix, chat, x.__mod_name__.replace(" ", "_").lower()
+                        prefix, chat, x.__mod_name__.lower()
                     ),
                 )
                 for x in module_dict.values()
             ]
-             )
-
-    pairs = list(zip(modules[::3], modules[1::3], modules[2::3]))
-    i = 0
-    for m in pairs:
-        for _ in m:
-            i += 1
-    if len(modules) - i == 1:
-        pairs.append((modules[-1],))
-    elif len(modules) - i == 2:
-        pairs.append(
-            (
-                modules[-2],
-                modules[-1],
-            )
         )
 
-    COLUMN_SIZE = 4
+    pairs = [modules[i * 3 : (i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)]
 
-    max_num_pages = ceil(len(pairs) / COLUMN_SIZE)
+    round_num = len(modules) / 3
+    calc = len(modules) - round(round_num)
+    if calc in [1, 2]:
+        pairs.append((modules[-1],))
+
+    max_num_pages = ceil(len(pairs) / 6)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
-    if len(pairs) > COLUMN_SIZE:
-        pairs = pairs[
-            modulo_page * COLUMN_SIZE : COLUMN_SIZE * (modulo_page + 1)
-        ] + [
+    if len(pairs) > 3:
+        pairs = pairs[modulo_page * 6 : 6 * (modulo_page + 1)] + [
             (
                 EqInlineKeyboardButton(
-                    "❮",
-                    callback_data="{}_prev({})".format(prefix, modulo_page),
+                    "❮", callback_data="{}_prev({})".format(prefix, modulo_page)
                 ),
+                EqInlineKeyboardButton("Back", callback_data="Shikimori_back"),
                 EqInlineKeyboardButton(
-                    "Back",
-                    callback_data="Shikimori_back",
-                ),
-                EqInlineKeyboardButton(
-                    "❯",
-                    callback_data="{}_next({})".format(prefix, modulo_page),
+                    "❯", callback_data="{}_next({})".format(prefix, modulo_page)
                 ),
             )
         ]
@@ -131,6 +119,7 @@ def article(
         ),
         reply_markup=reply_markup,
     )
+
 
 def send_to_list(
     bot: Bot, send_to: list, message: str, markdown=False, html=False
@@ -161,12 +150,14 @@ def build_keyboard(buttons):
 
 
 def revert_buttons(buttons):
-    return "".join(
-        "\n[{}](buttonurl://{}:same)".format(btn.name, btn.url)
-        if btn.same_line
-        else "\n[{}](buttonurl://{})".format(btn.name, btn.url)
-        for btn in buttons
-    )
+    res = ""
+    for btn in buttons:
+        if btn.same_line:
+            res += "\n[{}](buttonurl://{}:same)".format(btn.name, btn.url)
+        else:
+            res += "\n[{}](buttonurl://{})".format(btn.name, btn.url)
+
+    return res
 
 
 def build_keyboard_parser(bot, chat_id, buttons):
@@ -182,16 +173,67 @@ def build_keyboard_parser(bot, chat_id, buttons):
     return keyb
 
 
+def user_bot_owner(func):
+    @wraps(func)
+    def is_user_bot_owner(bot: Bot, update: Update, *args, **kwargs):
+        user = update.effective_user
+        if user and user.id == OWNER_ID:
+            return func(bot, update, *args, **kwargs)
+        else:
+            pass
+
+    return is_user_bot_owner
+
+
+def convert_gif(input):
+    """Function to convert mp4 to webm(vp9)"""
+
+    vid = cv2.VideoCapture(input)
+    height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+
+    #check height and width to scale
+    if width > height:
+        width = 512
+        height = -1
+    elif height > width:
+        height = 512
+        width = -1
+    elif width == height:
+        width = 512
+        height = 512
+
+
+    converted_name = 'kangsticker.webm'
+
+    (
+        ffmpeg
+            .input(input)
+            .filter('fps', fps=30, round="up")
+            .filter('scale', width=width, height=height)
+            .trim(start="00:00:00", end="00:00:03", duration="3")
+            .output(converted_name, vcodec="libvpx-vp9", 
+                        **{
+                            #'vf': 'scale=512:-1',
+                            'crf': '30'
+                            })
+            .overwrite_output()
+            .run()
+    )
+
+    return converted_name
+
+
+def build_keyboard_alternate(buttons):
+    keyb = []
+    for btn in buttons:
+        if btn[2] and keyb:
+            keyb[-1].append(InlineKeyboardButton(btn[0], url=btn[1]))
+        else:
+            keyb.append([InlineKeyboardButton(btn[0], url=btn[1])])
+
+    return keyb
+
+
 def is_module_loaded(name):
     return name not in NO_LOAD
-
-def upload_text(data: str) -> typing.Optional[str]:
-    compress = zlib.compressobj(wbits=-15)
-    paste_blob = compress.compress(json.dumps({'paste': data}, separators=(',', ':')).encode()) + compress.flush()
-    paste_meta = [[base64.b64encode(cipher.nonce).decode(), base64.b64encode(salt).decode(), 100000, 256, 128, 'gcm', 'zlib'], 'syntaxhighlighting', 0, 0]
-    cipher.update(json.dumps(paste_meta, separators=(',', ':')).encode())
-    ct, tag = cipher.encrypt_and_digest(paste_blob)
-    resp = requests.post('https://bin.nixnet.services', headers={'X-Requested-With': 'JSONHttpRequest'}, data=json.dumps({'v': 2, 'adata': paste_meta, 'ct': base64.b64encode(ct + tag).decode(), 'meta': {'expire': '1week'}}, separators=(',', ':')))
-    data = resp.json()
-    url = list(urlparse(urljoin('https://bin.nixnet.services', data['url'])))
-    return urlunparse(url)
