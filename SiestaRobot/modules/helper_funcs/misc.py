d3ec5bd02fd1c58f08a00dd216c1377d75f54759
Makima <105355430+Control-Devil@users.noreply.github.com>
2022-06-08 14:45:55 +0530

    Update misc.py

--- SiestaRobot/modules/helper_funcs/misc.py
+++ SiestaRobot/modules/helper_funcs/misc.py
@@ -90,7 +90,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
                 EqInlineKeyboardButton(
                     "❮", callback_data="{}_prev({})".format(prefix, modulo_page)
                 ),
-                EqInlineKeyboardButton("Back", callback_data="shikimori_back"),
+                EqInlineKeyboardButton("Back", callback_data="help_back"),
                 EqInlineKeyboardButton(
                     "❯", callback_data="{}_next({})".format(prefix, modulo_page)
                 ),
@@ -98,7 +98,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         ]
 
     else:
-        pairs += [[EqInlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="shikimori_back")]]
+        pairs += [[EqInlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="help_back")]]
 
     return pairs
 
