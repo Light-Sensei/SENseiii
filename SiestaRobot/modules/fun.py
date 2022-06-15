4db7338f66787d140c35e8bd5c9fa99720fc563a
Light Yagami <99256366+Light-Sensei@users.noreply.github.com>
2022-06-15 23:18:36 +0530

    Update fun.py

--- SiestaRobot/modules/fun.py
+++ SiestaRobot/modules/fun.py
@@ -318,147 +318,6 @@ def helps(chat):
     return gs(chat, "fun_help")
 
 
-def gbam(update, context):
-    user = update.effective_user
-    chat = update.effective_chat
-    bot, args = context.bot, context.args
-    message = update.effective_message
-
-    curr_user = html.escape(message.from_user.first_name)
-    user_id = extract_user(message, args)
-
-    if user_id:
-        gbam_user = bot.get_chat(user_id)
-        user1 = curr_user
-        user2 = html.escape(gbam_user.first_name)
-
-    else:
-        user1 = curr_user
-        user2 = bot.first_name
-
-    if update.effective_message.chat.type == "private":
-        return
-    if int(user.id) in DRAGONS or int(user.id) in DEMONS:
-        gbamm = fun_strings.GBAM
-        reason = random.choice(fun_strings.GBAM_REASON)
-        gbam = gbamm.format(user1=user1, user2=user2, chatid=chat.id, reason=reason)
-        context.bot.sendMessage(chat.id, gbam, parse_mode=ParseMode.HTML)
-	
-def flirt(update: Update, context: CallbackContext):
-    reply_text = update.effective_message.reply_to_message.reply_text if update.effective_message.reply_to_message else update.effective_message.reply_text
-    reply_text(random.choice(fun_strings.FLIRT_TEXT))
-	
-def romance(update: Update, context: CallbackContext):
-	bot = context.bot
-	args = context.args
-	message = update.effective_message
-
-	reply_to = message.reply_to_message or message
-
-	curr_user = html.escape(message.from_user.first_name)
-	user_id = extract_user(message, args)
-
-	if user_id:
-	    romantic_user = bot.get_chat(user_id)
-	    user1 = curr_user
-	    user2 = html.escape(romantic_user.first_name)
-
-	else:
-	    user1 = bot.first_name
-	    user2 = curr_user
-
-	romance_type = random.choice(("Text", "Gif", "Sticker"))
-	if romance_type == "Gif":
-	    try:
-	        temp = random.choice(fun_strings.ROMANCE_GIFS)
-	        reply_to.reply_animation(temp)
-	    except BadRequest:
-	        romance_type = "Text"
-
-	if romance_type == "Sticker":
-	    try:
-	        temp = random.choice(fun_strings.ROMANCE_STICKERS)
-	        reply_to.reply_sticker(temp)
-	    except BadRequest:
-	        romance_type = "Text"
-
-	if romance_type == "Text":
-	    temp = random.choice(fun_strings.ROMANCE_TEMPLATES)
-	    reply = temp.format(user1=user1, user2=user2)
-	    reply_to.reply_text(reply, parse_mode=ParseMode.HTML)
-	
-def owo(update: Update, context: CallbackContext):
-	bot = context.bot
-	args = context.args
-	message = update.effective_message
-
-	reply_to = message.reply_to_message or message
-
-	curr_user = html.escape(message.from_user.first_name)
-	user_id = extract_user(message, args)
-
-	if user_id:
-	    owo_user = bot.get_chat(user_id)
-	    user1 = curr_user
-	    user2 = html.escape(owo_user.first_name)
-
-	else:
-	    user1 = bot.first_name
-	    user2 = curr_user
-
-	owo_type = random.choice(("Gif", "Sticker"))
-	if owo_type == "Gif":
-	    try:
-	        temp = random.choice(fun_strings.OWO_GIFS)
-	        reply_to.reply_animation(temp)
-	    except BadRequest:
-	        owo_type = "Text"
-
-	if owo_type == "Sticker":
-	    try:
-	        temp = random.choice(fun_strings.OWO_STICKERS)
-	        reply_to.reply_sticker(temp)
-	    except BadRequest:
-	        owo_type = "Text"
-
-def uwu(update: Update, context: CallbackContext):
-	bot = context.bot
-	args = context.args
-	message = update.effective_message
-
-	reply_to = message.reply_to_message or message
-
-	curr_user = html.escape(message.from_user.first_name)
-	user_id = extract_user(message, args)
-
-	if user_id:
-	    uwu_user = bot.get_chat(user_id)
-	    user1 = curr_user
-	    user2 = html.escape(uwu_user.first_name)
-
-	else:
-	    user1 = bot.first_name
-	    user2 = curr_user
-
-	uwu_type = random.choice(("Gif", "Sticker"))
-	if uwu_type == "Gif":
-	    try:
-	        temp = random.choice(fun_strings.UWU_GIFS)
-	        reply_to.reply_animation(temp)
-	    except BadRequest:
-	        uwu_type = "Text"
-
-	if uwu_type == "Sticker":
-	    try:
-	        temp = random.choice(fun_strings.UWU_STICKERS)
-	        reply_to.reply_sticker(temp)
-	    except BadRequest:
-	        uwu_type = "Text"
-	
-def sex(update: Update, context: CallbackContext):
-    reply_text = update.effective_message.reply_to_message.reply_text if update.effective_message.reply_to_message else update.effective_message.reply_text
-    reply_text(random.choice(fun_strings.SEX_TEXT))
-
 SANITIZE_HANDLER = DisableAbleCommandHandler("sanitize", sanitize, run_async=True)
 RUNS_HANDLER = DisableAbleCommandHandler("runs", runs, run_async=True)
 SLAP_HANDLER = DisableAbleCommandHandler("slap", slap, run_async=True)
@@ -474,11 +333,6 @@ EIGHTBALL_HANDLER = DisableAbleCommandHandler("8ball", eightball, run_async=True
 TABLE_HANDLER = DisableAbleCommandHandler("table", table, run_async=True)
 SHOUT_HANDLER = DisableAbleCommandHandler("shout", shout, run_async=True)
 WEEBIFY_HANDLER = DisableAbleCommandHandler("weebify", weebify, run_async=True)
-GBAM_HANDLER = DisableAbleCommandHandler("gbam", gbam, run_async=True)
-ROMANCE_HANDLER = DisableAbleCommandHandler("romance", romance, run_async=True)
-OWO_HANDLER = DisableAbleCommandHandler("owo", owo, run_async=True)
-SEX_HANDLER = DisableAbleCommandHandler("sex", sex, run_async=True)
-UWU_HANDLER = DisableAbleCommandHandler("uwu", uwu, run_async=True)
 
 dispatcher.add_handler(WEEBIFY_HANDLER)
 dispatcher.add_handler(SHOUT_HANDLER)
@@ -496,10 +350,6 @@ dispatcher.add_handler(DECIDE_HANDLER)
 dispatcher.add_handler(ROMANCE_HANDLER)
 dispatcher.add_handler(EIGHTBALL_HANDLER)
 dispatcher.add_handler(TABLE_HANDLER)
-dispatcher.add_handler(GBAM_HANDLER)
-dispatcher.add_handler(OWO_HANDLER)
-dispatcher.add_handler(UWU_HANDLER)
-dispatcher.add_handler(SEX_HANDLER)
 
 __mod_name__ = "Fun"
 __command_list__ = [
@@ -542,9 +392,5 @@ __handlers__ = [
     SHOUT_HANDLER,
     ROMANCE_HANDLER,
     WEEBIFY_HANDLER,
-    SEX_HANDLER,	
-    UWU_HANDLER,
-    OWO_HANDLER,
     EIGHTBALL_HANDLER,
-    GBAM_HANDLER,
 ]
